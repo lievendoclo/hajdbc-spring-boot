2c90b01a7a9d8a8ee888295eb71f7c3777c50ed4
Lieven Doclo <lieven.doclo@lightspeedpos.com>
2015-07-06 11:48:25 +0200

    simplified configuration and readme.

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # hajdbc-spring-boot
+
 Autoconfiguration of HA-JDBC components for Spring Boot
+
+# Usage
+
+Just include this project as as dependency in your Spring Boot project and add the configuration
+for the backend databases in your application properties.
+
+## YAML configuration
+```
+hajdbc:
+    driverDatabases:
+        - id: db1
+          location: jdbc:hsqldb:mem:db1
+          user: sa
+        - id: db1
+          location: jdbc:hsqldb:mem:db1
+          user: sa
+spring:
+    datasource:
+        url: jdbc:ha-jdbc:default
+        username: sa
+```
+
+## Properties file configuration
+```
+hajdbc.driverDatabases[0].id=db1
+hajdbc.driverDatabases[0].location=jdbc:hsqldb:mem:db1
+hajdbc.driverDatabases[0].user=sa
+hajdbc.driverDatabases[1].id=db2
+hajdbc.driverDatabases[1].location=jdbc:hsqldb:mem:db2
+hajdbc.driverDatabases[1].user=sa
+
+spring.datasource.url=jdbc:ha-jdbc:default
+spring.datasource.username=sa
+```
+
+## Overriding default HA-JDBC factories
+
+By default, this integration uses default factory implementations for the state manager,
+database metadata cache and balancer. If you provide your own implementation as a bean,
+the integration will pick those over the default. If for example you want to use a SQLite
+state manager, just create a bean like this in your configuration:
+
+```
+@Bean
+public StateManagerFactory stateManagerFactory() {
+    return new SQLiteStateManagerFactory();
+}
+```
+
+By default. the integration will use a SimpleStateManagerFactory, a RoundRobinBalancerFactory
+and a SharedEagerDatabaseMetaDataCacheFactory.
+
+## Configurable properties
+
+All configurable properties are prefixed with hajdbc. The following properties are configurable for this integration:
+
+- **enabled**: Whether HA JDBC should be enabled. Default is true
+- **driverDatabases**: The backend databases for HA JDBC. Required to be configured.
+- **clusterName**: The name of the HA JDBC cluster, used in the ha-jdbc jdbc url. Default is "default"
+- **cronExpression**: The cron expression that indicates when synchronization should occur
+- **defaultSynchronizationStrategy**: Which synchronization strategy should be chosen. Possible values are
+"full", "fastdiff", "diff", "dump-restore", "per-table" and "passive". Default is "full"
+- **enableIdentityColumnDetection**: Whether identity column detection is enabled. Default is true
+- **enableSequenceDetection**: Whether sequence detection is enabled. Default is true
+
--- src/main/java/be/insaneprogramming/springboot/hajdbc/HaJdbcAutoConfiguration.java
+++ src/main/java/be/insaneprogramming/springboot/hajdbc/HaJdbcAutoConfiguration.java
@@ -3,22 +3,14 @@ package be.insaneprogramming.springboot.hajdbc;
 import net.sf.hajdbc.SimpleDatabaseClusterConfigurationFactory;
 import net.sf.hajdbc.SynchronizationStrategy;
 import net.sf.hajdbc.balancer.BalancerFactory;
-import net.sf.hajdbc.balancer.random.RandomBalancerFactory;
-import net.sf.hajdbc.balancer.roundrobin.RoundRobinBalancerFactory;
 import net.sf.hajdbc.balancer.simple.SimpleBalancerFactory;
 import net.sf.hajdbc.cache.DatabaseMetaDataCacheFactory;
-import net.sf.hajdbc.cache.eager.EagerDatabaseMetaDataCacheFactory;
 import net.sf.hajdbc.cache.eager.SharedEagerDatabaseMetaDataCacheFactory;
-import net.sf.hajdbc.cache.lazy.LazyDatabaseMetaDataCacheFactory;
-import net.sf.hajdbc.cache.lazy.SharedLazyDatabaseMetaDataCacheFactory;
-import net.sf.hajdbc.cache.simple.SimpleDatabaseMetaDataCacheFactory;
 import net.sf.hajdbc.sql.Driver;
 import net.sf.hajdbc.sql.DriverDatabase;
 import net.sf.hajdbc.sql.DriverDatabaseClusterConfiguration;
-import net.sf.hajdbc.state.bdb.BerkeleyDBStateManagerFactory;
+import net.sf.hajdbc.state.StateManagerFactory;
 import net.sf.hajdbc.state.simple.SimpleStateManagerFactory;
-import net.sf.hajdbc.state.sql.SQLStateManagerFactory;
-import net.sf.hajdbc.state.sqlite.SQLiteStateManagerFactory;
 import net.sf.hajdbc.sync.DifferentialSynchronizationStrategy;
 import net.sf.hajdbc.sync.DumpRestoreSynchronizationStrategy;
 import net.sf.hajdbc.sync.FastDifferentialSynchronizationStrategy;
@@ -29,11 +21,14 @@ import net.sf.hajdbc.util.concurrent.cron.CronExpression;
 
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.text.ParseException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -51,6 +46,15 @@ import lombok.Data;
 @Configuration
 @Data
 public class HaJdbcAutoConfiguration {
+    private static final List<SynchronizationStrategy> SYNCHRONIZATION_STRATEGIES = Arrays.asList(
+        new FullSynchronizationStrategy(),
+        new DumpRestoreSynchronizationStrategy(),
+        new DifferentialSynchronizationStrategy(),
+        new FastDifferentialSynchronizationStrategy(),
+        new PerTableSynchronizationStrategy(new FullSynchronizationStrategy()),
+        new PassiveSynchronizationStrategy()
+    );
+
     /**
      * Enable or disable HA JDBC integration
      * **/
@@ -69,37 +73,9 @@ public class HaJdbcAutoConfiguration {
      */
     String cronExpression = "0 0/1 * 1/1 * ? *";
     /**
-     * Which balancer factory should be used for queries
-     */
-    String balancerFactory = "round-robin";
-    /**
      * What synchronization factory should be used
      */
     String defaultSynchronizationStrategy = "full";
-    /**
-     * Indicates how the meta data for HA JDBC should be cached
-     */
-    String databaseMetaDataCacheFactory = "shared-eager";
-    /**
-     * How the state of the cluster should be managed
-     */
-    String stateManagerFactory = "simple";
-    /**
-     * The JDBC URL for an SQL state manager
-     */
-    String stateManagerUrlPattern;
-    /**
-     * The user name for am SQL state manager
-     */
-    String stateManagerUser;
-    /**
-     * The user password for an SQL state manager
-     */
-    String stateManagerPassword;
-    /**
-     * The location pattern for an SqLite or BerkeleyDB state manager
-     */
-    String stateManagerLocationPattern;
     /**
      * Whether identity column detection should be enabled
      */
@@ -108,6 +84,24 @@ public class HaJdbcAutoConfiguration {
      * Whether sequence detection should be enabled
      */
     boolean sequenceDetectionEnabled = true;
+
+    @Bean
+    @ConditionalOnMissingBean
+    public BalancerFactory balancerFactory() {
+        return new SimpleBalancerFactory();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public StateManagerFactory stateManagerFactory() {
+        return new SimpleStateManagerFactory();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public DatabaseMetaDataCacheFactory databaseMetaDataCacheFactory() {
+        return new SharedEagerDatabaseMetaDataCacheFactory();
+    }
 
     @PostConstruct
     void register() throws ParseException {
@@ -118,37 +112,10 @@ public class HaJdbcAutoConfiguration {
             } else {
                 throw new IllegalStateException("HA JDBC driver databases should be configured to contain at least one driver database");
             }
-            config.setDatabaseMetaDataCacheFactory(DatabaseMetaDataCacheChoice.fromId(databaseMetaDataCacheFactory));
-            config.setBalancerFactory(BalancerChoice.fromId(balancerFactory));
-            switch (stateManagerFactory) {
-                case "sql":
-                    SQLStateManagerFactory sqlStateManagerFactory = new SQLStateManagerFactory();
-                    if (stateManagerUrlPattern != null)
-                        sqlStateManagerFactory.setUrlPattern(stateManagerUrlPattern);
-                    if (stateManagerUser != null)
-                        sqlStateManagerFactory.setUser(stateManagerUser);
-                    if (stateManagerPassword != null)
-                        sqlStateManagerFactory.setPassword(stateManagerPassword);
-                    config.setStateManagerFactory(sqlStateManagerFactory);
-                    break;
-
-                case "berkeleydb":
-                    BerkeleyDBStateManagerFactory
-                        berkeleyDBStateManagerFactory = new BerkeleyDBStateManagerFactory();
-                    if (stateManagerLocationPattern != null)
-                        berkeleyDBStateManagerFactory.setLocationPattern(stateManagerLocationPattern);
-                    config.setStateManagerFactory(berkeleyDBStateManagerFactory);
-                    break;
-                case "sqlite":
-                    SQLiteStateManagerFactory sqLiteStateManagerFactory = new SQLiteStateManagerFactory();
-                    if (stateManagerLocationPattern != null)
-                        sqLiteStateManagerFactory.setLocationPattern(stateManagerLocationPattern);
-                    config.setStateManagerFactory(sqLiteStateManagerFactory);
-                    break;
-                default:
-                    config.setStateManagerFactory(new SimpleStateManagerFactory());
-            }
-            config.setSynchronizationStrategyMap(SynchronizationStrategyChoice.getIdMap());
+            config.setDatabaseMetaDataCacheFactory(databaseMetaDataCacheFactory());
+            config.setBalancerFactory(balancerFactory());
+            config.setStateManagerFactory(stateManagerFactory());
+            config.setSynchronizationStrategyMap(getSynchronizationStrategyMap());
             config.setDefaultSynchronizationStrategy(defaultSynchronizationStrategy);
             config.setIdentityColumnDetectionEnabled(identityColumnDetectionEnabled);
             config.setSequenceDetectionEnabled(sequenceDetectionEnabled);
@@ -159,81 +126,11 @@ public class HaJdbcAutoConfiguration {
         }
     }
 
-    private enum DatabaseMetaDataCacheChoice {
-        SIMPLE(new SimpleDatabaseMetaDataCacheFactory()),
-        LAZY(new LazyDatabaseMetaDataCacheFactory()),
-        EAGER(new EagerDatabaseMetaDataCacheFactory()),
-        SHARED_LAZY(new SharedLazyDatabaseMetaDataCacheFactory()),
-        SHARED_EAGER(new SharedEagerDatabaseMetaDataCacheFactory());
-
-        DatabaseMetaDataCacheFactory databaseMetaDataCacheFactory;
-
-        DatabaseMetaDataCacheChoice(DatabaseMetaDataCacheFactory databaseMetaDataCacheFactory) {
-            this.databaseMetaDataCacheFactory = databaseMetaDataCacheFactory;
+    private static Map<String, SynchronizationStrategy> getSynchronizationStrategyMap() {
+        Map<String, SynchronizationStrategy> map = new HashMap<>();
+        for (SynchronizationStrategy synchronizationStrategy : SYNCHRONIZATION_STRATEGIES) {
+            map.put(synchronizationStrategy.getId(), synchronizationStrategy);
         }
-
-        static DatabaseMetaDataCacheFactory fromId(String id) {
-            for (DatabaseMetaDataCacheChoice databaseMetaDataCacheChoice : DatabaseMetaDataCacheChoice.values()) {
-                if(databaseMetaDataCacheChoice.databaseMetaDataCacheFactory.getId().equals(id)) {
-                    return databaseMetaDataCacheChoice.databaseMetaDataCacheFactory;
-                }
-            }
-            throw new IllegalArgumentException("Could not find database metadata cache factory with id " + id);
-        }
-    }
-
-    private enum BalancerChoice {
-        ROUND_ROBIN(new RoundRobinBalancerFactory()),
-        RANDOM(new RandomBalancerFactory()),
-        SIMPLE(new SimpleBalancerFactory());
-
-        BalancerFactory balancerFactory;
-
-        BalancerChoice(BalancerFactory balancerFactory) {
-            this.balancerFactory = balancerFactory;
-        }
-
-        static BalancerFactory fromId(String id) {
-            for (BalancerChoice balancerChoice : BalancerChoice.values()) {
-                if(balancerChoice.balancerFactory.getId().equals(id)) {
-                    return balancerChoice.balancerFactory;
-                }
-            }
-            throw new IllegalArgumentException("Could not find balancer factory with id " + id);
-        }
-    }
-
-    private enum SynchronizationStrategyChoice {
-        FULL(new FullSynchronizationStrategy()),
-        DUMP_RESTORE(new DumpRestoreSynchronizationStrategy()),
-        DIFF(new DifferentialSynchronizationStrategy()),
-        FASTDIFF(new FastDifferentialSynchronizationStrategy()),
-        PER_TABLE_FULL(new PerTableSynchronizationStrategy(new FullSynchronizationStrategy())),
-        PER_TABLE_DIFF(new PerTableSynchronizationStrategy(new DifferentialSynchronizationStrategy())),
-        PASSIVE(new PassiveSynchronizationStrategy());
-
-        SynchronizationStrategy synchronizationStrategy;
-
-        SynchronizationStrategyChoice(SynchronizationStrategy synchronizationStrategy) {
-            this.synchronizationStrategy = synchronizationStrategy;
-        }
-
-        static SynchronizationStrategy fromId(String id) {
-            for (SynchronizationStrategyChoice synchronizationStrategyChoice : SynchronizationStrategyChoice.values()) {
-                if(synchronizationStrategyChoice.synchronizationStrategy.getId().equals(id)) {
-                    return synchronizationStrategyChoice.synchronizationStrategy;
-                }
-            }
-            throw new IllegalArgumentException("Could not find synchronization strategy with id " + id);
-        }
-
-        static Map<String, SynchronizationStrategy> getIdMap() {
-            Map<String, SynchronizationStrategy> strategies = new HashMap<>();
-            for (SynchronizationStrategyChoice synchronizationStrategyChoice : SynchronizationStrategyChoice.values()) {
-                strategies.put(synchronizationStrategyChoice.synchronizationStrategy.getId(),
-                               synchronizationStrategyChoice.synchronizationStrategy);
-            }
-            return strategies;
-        }
+        return map;
     }
 }
